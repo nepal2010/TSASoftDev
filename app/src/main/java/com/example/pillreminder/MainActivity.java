1df39dcb1297d47ddd1e48fadfc868c817c7489b
razatsutradhar <razat74@gmail.com>
2020-01-28 22:20:57 -0500

    prototype adding schedule to the pill reminder

--- app/src/main/java/com/example/pillreminder/MainActivity.java
+++ app/src/main/java/com/example/pillreminder/MainActivity.java
@@ -12,28 +12,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.pillreminder.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner hourSpinner, minuteSpinner, timeHemisphereSpinner;
+    ArrayAdapter<CharSequence> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        timeHemisphereSpinner = findViewById(R.id.AMPMDropDown);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.timeHemisphere_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeHemisphereSpinner.setAdapter(adapter);
+
+        hourSpinner = findViewById(R.id.hourDropDown);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.hours_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hourSpinner.setAdapter(adapter);
+
+        minuteSpinner = findViewById(R.id.minuteDropDown);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.minutes_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minuteSpinner.setAdapter(adapter);
+
+
+
     }
 }
