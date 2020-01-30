1a900eea4a0572a727f2be2379c24410e2692ff2
razatsutradhar <razat74@gmail.com>
2020-01-29 19:45:21 -0500

    added the calendar and buttons to select dates

--- app/src/main/java/com/example/pillreminder/MainActivity.java
+++ app/src/main/java/com/example/pillreminder/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.pillreminder;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +16,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.Spinner;
 
 import com.example.pillreminder.ui.main.SectionsPagerAdapter;
@@ -20,6 +25,10 @@ import com.example.pillreminder.ui.main.SectionsPagerAdapter;
 public class MainActivity extends AppCompatActivity {
     Spinner hourSpinner, minuteSpinner, timeHemisphereSpinner;
     ArrayAdapter<CharSequence> adapter;
+    Button fromDateButton, toDateButton;
+    boolean isSelectingFromDate, isSelectingToDate = false;
+    CalendarView dateSelectorCaleder;
+    ColorInt selectedColor, unselectedColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +52,57 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         minuteSpinner.setAdapter(adapter);
 
+        dateSelectorCaleder = findViewById(R.id.caledarView);
+        dateSelectorCaleder.setVisibility(View.INVISIBLE);
+
+        fromDateButton = findViewById(R.id.fromDate);
+        fromDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSelectingFromDate = true;
+                fromDateButton.setBackgroundColor(Color.GREEN);
+
+                isSelectingToDate = false;
+                toDateButton.setBackgroundColor(Color.GRAY);
+
+                dateSelectorCaleder.setVisibility(View.VISIBLE);
+
+            }
+        });
 
 
+        toDateButton = findViewById(R.id.toDate);
+        toDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isSelectingToDate = true;
+                toDateButton.setBackgroundColor(Color.GREEN);
+
+                isSelectingFromDate = false;
+                fromDateButton.setBackgroundColor(Color.GRAY);
+
+                dateSelectorCaleder.setVisibility(View.VISIBLE);
+            }
+        });
+
+        dateSelectorCaleder.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                if(isSelectingFromDate){
+                    fromDateButton.setText("" + (i1+1) + " / " + i2 + " / "+ i);
+                }else if(isSelectingToDate){
+                    toDateButton.setText("" + (i1+1) + " / " + i2 + " / "+ i);
+                }
+                isSelectingFromDate = false;
+                isSelectingToDate = false;
+
+                fromDateButton.setBackgroundColor(Color.GRAY);
+                toDateButton.setBackgroundColor(Color.GRAY);
+
+                dateSelectorCaleder.setVisibility(View.INVISIBLE);
+            }
+        });
     }
+
 }
+
