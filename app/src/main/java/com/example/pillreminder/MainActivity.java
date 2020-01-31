5651b631ca4be066320edacbf07e5eb626721c4c
razatsutradhar <razat74@gmail.com>
2020-01-30 19:53:48 -0500

    worked with the calendar, colors, added the spinner for frequency of pills

--- app/src/main/java/com/example/pillreminder/MainActivity.java
+++ app/src/main/java/com/example/pillreminder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pillreminder;
 
 import android.graphics.Color;
+import android.opengl.Visibility;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,9 +10,11 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,13 +25,18 @@ import android.widget.Spinner;
 
 import com.example.pillreminder.ui.main.SectionsPagerAdapter;
 
+import java.security.Principal;
+import java.security.acl.Group;
+import java.util.Enumeration;
+
 public class MainActivity extends AppCompatActivity {
-    Spinner hourSpinner, minuteSpinner, timeHemisphereSpinner;
+    Spinner hourSpinner, minuteSpinner, timeHemisphereSpinner, frequencySpinner;
     ArrayAdapter<CharSequence> adapter;
     Button fromDateButton, toDateButton;
     boolean isSelectingFromDate, isSelectingToDate = false;
     CalendarView dateSelectorCaleder;
     ColorInt selectedColor, unselectedColor;
+    Group underCalendarGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +60,16 @@ public class MainActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         minuteSpinner.setAdapter(adapter);
 
+        frequencySpinner = findViewById(R.id.frequencySpinner);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.frequency_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        frequencySpinner.setAdapter(adapter);
+
         dateSelectorCaleder = findViewById(R.id.caledarView);
         dateSelectorCaleder.setVisibility(View.INVISIBLE);
 
+        underCalendarGroup = findViewById(R.id.underCalendarGroup);
         fromDateButton = findViewById(R.id.fromDate);
         fromDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,6 +118,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
 }
 
